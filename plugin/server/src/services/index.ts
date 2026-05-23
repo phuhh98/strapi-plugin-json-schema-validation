419da3c797546862e1b35780d0ae55e9c3dd22f9
phuhh98 <phuhh98@gmail.com>
2026-05-23 13:16:40 +0700

    chores(server): remove template service and controller

--- plugin/server/src/services/index.ts
+++ plugin/server/src/services/index.ts
@@ -1,17 +1,13 @@
-import service, { Service } from './service';
 import validation, { ValidationService } from './validation';
 
 export enum ServiceName {
-  Service = 'service',
   Validation = 'validation',
 }
 
 export type Services = {
-  [ServiceName.Service]: Service;
   [ServiceName.Validation]: ValidationService;
 };
 
 export default {
-  [ServiceName.Service]: service,
   [ServiceName.Validation]: validation,
 };
